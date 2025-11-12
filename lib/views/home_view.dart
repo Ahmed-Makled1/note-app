773bdd8403d51ddfb00c7e447b586b9cdcbcd833
Ahmed Makled <ahmed1.makled1@gmail.com>
2025-11-12 20:22:24 +0200

    add showModalBottomSheet

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
 import 'package:note_app/widgets/note_list_view.dart';
+import 'package:note_app/widgets/custom_bottom_sheet.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,12 +11,18 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 24, right: 24),
-        child: Column(
-          children: [
-            CustomAppBar(),
-            NoteListView(),
-          ],
-        ),
+        child: Column(children: [CustomAppBar(), NoteListView()]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return CustomBottomSheet();
+            },
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
---
+++ lib/widgets/custom_bottom_sheet.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomSheet extends StatelessWidget {
+  const CustomBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
