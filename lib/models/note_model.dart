d4d8af7c5175e0a609f013bcde81546d5e5e7f09
Ahmed Makled <ahmed1.makled1@gmail.com>
2025-11-18 15:20:34 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NoteModel {
+  final String title;
+  final String descrition;
+  final MaterialColor color;
+
+  NoteModel({required this.title, required this.descrition, required this.color});
+}
